1bb03653c94a6a5e46f2c87cbf6c9af8d6de87fe
Felipe de Oliveira <felipe.auol@gmail.com>
2017-08-18 23:24:21 -0400

    Added page selection for nav bar

--- public/manage_contents.php
+++ public/manage_contents.php
@@ -1,7 +1,17 @@
 <?php include("../includes/layouts/header.html")?>
 <?php require_once("../includes/functions.php")?>
 
+<?php
 
+if(isset($_GET["subject"])){
+    $selected_subject_id = $_GET["subject"];
+    $selected_page_id = null;
+}elseif (isset($_GET["page"])){
+    $selected_page_id = $_GET["page"];
+    $selected_subject_id = null;
+}else $selected_page_id = null && $selected_page_id = null;
+
+?>
 <div id="navigation" class="subjects">
     <ul class="subjects">
                 <?php
@@ -12,16 +22,23 @@
                     while($subject_row = mysqli_fetch_assoc($subject_set)) {
                 ?>
 
-            <li>    <?php echo $subject_row['menu_name'] ?>
+            <li>    <a href="manage_contents.php?subject=<?php echo urlencode($subject_row['id'])?>">
+                        <?php echo $subject_row['menu_name'] ?>
+                    </a>
 
                     <?php
                     $page_set = find_pages_for_subject($subject_row['id']);
                     ?>
 
                 <ul class="pages">
-                <?php while($page_row = mysqli_fetch_assoc($page_set)) {?>
+                    <?php while($page_row = mysqli_fetch_assoc($page_set)) {?>
 
-                        <li><?php echo $page_row['menu_name'];} ?></li>
+                        <li>
+                            <a href="manage_contents.php?page=<?php echo urlencode($page_row['id'])?>">
+                            <?php echo $page_row['menu_name'];} ?>
+                            </a>
+                        </li>
+
                 </ul>
 
             </li>
@@ -32,7 +49,8 @@
     </ul>
         <div id="page">
             <h2>Manage Content</h2>
-
+                <?php echo $selected_page_id; ?>
+                <?php echo $selected_subject_id; ?> <br />
             <main>
 
             </main>
