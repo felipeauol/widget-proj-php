ad1a18834ee46705416568ad73516fdf91557e81
Felipe de Oliveira <felipe.auol@gmail.com>
2017-08-19 14:02:55 -0400

    Refactored navigation code

--- includes/functions.php
+++ includes/functions.php
@@ -1,11 +1,9 @@
 <?php
-
 	function confirm_query($result_set) {
 		if (!$result_set) {
 			die("Database query failed.");
 		}
 	}
-
 	function find_all_subjects(){
        	global $connection;
 
@@ -19,7 +17,6 @@
 
         return $subject_set;
 }
-
 	function find_pages_for_subject($subject_id){
 		global $connection;
 
@@ -34,3 +31,44 @@
 
         return $page_set;
 	}
+
+	function navigation($subject_id, $page_id)
+    {
+    $output = "<ul class=\"subjects\">";
+    $subject_set = find_all_subjects();
+
+        while ($subject_row = mysqli_fetch_assoc($subject_set)) {
+
+            $output .= "<li";
+
+            if ($subject_id == $subject_row['id']) {
+                $output .= " class=\"selected\"";
+            }
+            $output .= ">";
+            $output .= "<a href=\"manage_contents.php?subject=";
+            $output .= urlencode($subject_row['id']);
+            $output .= "\">";
+            $output .= $subject_row['menu_name'];
+            $output .= "</a>";
+
+            $page_set = find_pages_for_subject($subject_row['id']);
+
+
+            $output .= "<ul class=\"pages\">";
+            while ($page_row = mysqli_fetch_assoc($page_set)) {
+                $output .= "<li";
+                if ($page_id == $page_row['id']) {
+                    $output .= " class=\"selected\"";
+                }
+                $output .= ">";
+                $output .= "<a href=\"manage_contents.php?page=";
+                $output .= urlencode($page_row['id']);
+                $output .= "\">";
+                $output .= $page_row['menu_name'];
+                $output .= "</a></li>";
+            }
+            $output .= "</ul></li>";
+        }
+        $output .= "</ul>";
+        return $output;
+    }
--- public/manage_contents.php
+++ public/manage_contents.php
@@ -2,73 +2,29 @@
 <?php require_once("../includes/functions.php")?>
 
 <?php
-
 if(isset($_GET["subject"])){
     $selected_subject_id = $_GET["subject"];
     $selected_page_id = null;
-}elseif (isset($_GET["page"])){
-    $selected_page_id = $_GET["page"];
-    $selected_subject_id = null;
-}else $selected_page_id = null && $selected_page_id = null;
-
+    }elseif (isset($_GET["page"])){
+        $selected_page_id = $_GET["page"];
+        $selected_subject_id = null;
+    }else {
+        $selected_subject_id = null;
+        $selected_page_id = null;
+    }
 ?>
-<div id="navigation" class="subjects">
-    <ul>
-                <?php
-                    $subject_set = find_all_subjects();
-                ?>
-
-                <?php
-                    while($subject_row = mysqli_fetch_assoc($subject_set)) {
-                ?>
-
-                <?php
-                echo "<li";
-                if($selected_subject_id == $subject_row['id']){
-                    echo " class=\"selected\"";
-                }
-                echo ">";
-                ?>
-                        <a href="manage_contents.php?subject=<?php echo urlencode($subject_row['id'])?>">
-                        <?php echo $subject_row['menu_name'] ?>
-                    </a>
-
-                    <?php
-                    $page_set = find_pages_for_subject($subject_row['id']);
-                    ?>
-
-                <ul class="pages">
-                    <?php while($page_row = mysqli_fetch_assoc($page_set)) {?>
-
-                    <?php
-                    echo "<li";
-                    if($selected_page_id == $page_row['id']){
-                        echo " class=\"selected\"";
-                    }
-                    echo ">";
-                    ?>
-                            <a href="manage_contents.php?page=<?php echo urlencode($page_row['id'])?>">
-                            <?php echo $page_row['menu_name'];} ?>
-                            </a>
-                        </li>
-
-                </ul>
-
-            </li>
-        <?php
-            }
-        ?>
-        </div>
-    </ul>
+<div id="main">
+<div id="navigation">
+    <?php
+    echo navigation($selected_subject_id,$selected_page_id);
+?>
+</div>
         <div id="page">
             <h2>Manage Content</h2>
-<!--                --><?php //echo "pageid" . $selected_page_id; ?>
-<!--                --><?php //echo "subjectid" . $selected_subject_id; ?><!-- <br />-->
-            <main>
-
-            </main>
+                <?php echo "pageid" . $selected_page_id; ?> <br />
+                <?php echo "subjectid" . $selected_subject_id; ?>
         </div>
-
+</div>
 <?php include("../includes/layouts/footer.html")?>
 
 <?php mysqli_close($connection); ?>
