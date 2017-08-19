796a278b4cb434a804a8e7ad437aa13c2591682f
Felipe de Oliveira <felipe.auol@gmail.com>
2017-08-19 17:23:43 -0400

    Added form for subject creation

---
+++ public/new_subject.php
@@ -0,0 +1,54 @@
+<?php include("../includes/layouts/header.html")?>
+<?php require_once("../includes/functions.php")?>
+
+<?php
+if(isset($_GET["subject"])){
+    $selected_subject_id = $_GET["subject"];
+    $selected_page_id = null;
+    }elseif (isset($_GET["page"])){
+        $selected_page_id = $_GET["page"];
+        $selected_subject_id = null;
+    }else {
+        $selected_subject_id = null;
+        $selected_page_id = null;
+    }
+?>
+<div id="main">
+<div id="navigation">
+    <?php
+    echo navigation($selected_subject_id,$selected_page_id);
+?>
+</div>
+        <div id="form">
+            <h2>Create a Subject</h2>
+
+            <form action="create_subject.php" method="post">
+                <p>Menu name:
+                    <input type="text" name="menu_name" value="" />
+                </p>
+                <p>Position:
+                    <select name="position">
+                        <?php
+                        $subject_count = mysqli_num_rows(find_all_subjects());
+                        for($count=1; $count <= ($subject_count + 2); $count++) {
+                            echo "<option value=\"{$count}\">{$count}</option>";
+                        }
+                        ?>
+                    </select>
+                </p>
+                <p>Visible:
+                    <input type="radio" name="visible" value="0" /> No
+                    &nbsp;
+                    <input type="radio" name="visible" value="1" /> Yes
+                </p>
+                <input type="submit" value="Create Subject" />
+            </form>
+            <br />
+            <a href="manage_contents.php">Cancel</a>
+
+
+        </div>
+</div>
+<?php include("../includes/layouts/footer.html")?>
+
+<?php mysqli_close($connection); ?>
